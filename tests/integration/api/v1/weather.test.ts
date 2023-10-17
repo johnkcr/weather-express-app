1c24c88a0c5fce614e2c3e2ddc3f2d80d2c1d81f
John Carter <johncarter7061@gmail.com>
2023-10-17 06:45:20 -0700

    Feat: Update testing

--- tests/integration/api/v1/weather.test.ts
+++ tests/integration/api/v1/weather.test.ts
@@ -1,12 +1,27 @@
 import request from 'supertest';
 import app from '../../../../src/app';
+import * as WeatherController from '../../../../src/controllers/weather.controller';
+
+// Mock the controller
+jest.mock('../../../../src/controllers/weather.controller');
 
 describe('Weather API', () => {
-    it('should fetch weather data by city and state', async () => {
-        const res = await request(app)
-            .get('/api/v1/weather')
-            .query({ lat: '3.5', lon: '4.5' });
-        
-        expect(res.status).toBe(200);
-    });
+
+  beforeEach(() => {
+      (WeatherController.getWeatherData as jest.Mock).mockClear();
+  });
+
+  it('should fetch weather data by city and state', async () => {
+      (WeatherController.getWeatherData as jest.Mock).mockImplementation((req, res) => {
+          res.status(200).json({ temperature: 25 });
+      });
+
+      const res = await request(app)
+          .get('/api/v1/weather')
+          .query({ lat: '3.5', lon: '4.5' });
+      
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({ temperature: 25 });
+  });
+
 });
